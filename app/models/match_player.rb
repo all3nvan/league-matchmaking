2aebd18eac630dc3721aa8827e656ed333a93b90
Allen Van <all3nvan@gmail.com>
2016-01-02 16:09:47 -0600

    all attributes required

--- app/models/match_player.rb
+++ app/models/match_player.rb
@@ -1,4 +1,8 @@
 class MatchPlayer < ActiveRecord::Base
   belongs_to :player
   belongs_to :match
+
+  validates :player_id, presence: true
+  validates :champion_id, presence: true
+  validates :match_id, presence: true
 end
--- spec/models/match_player_spec.rb
+++ spec/models/match_player_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe MatchPlayer, type: :model do
+  describe '#attributes' do
+    match_player = MatchPlayer.new
+
+    it 'has a player id' do
+      expect(match_player).to_not be_valid
+      expect(match_player.errors.keys).to include :player_id
+    end
+
+    it 'has a champion id' do
+      expect(match_player).to_not be_valid
+      expect(match_player.errors.keys).to include :champion_id
+    end
+
+    it 'has a match id' do
+      expect(match_player).to_not be_valid
+      expect(match_player.errors.keys).to include :match_id
+    end
+  end
 end
