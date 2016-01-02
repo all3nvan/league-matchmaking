0e2427467facbd08199e849e56b1432412e89e4a
Allen Van <all3nvan@gmail.com>
2016-01-02 16:11:56 -0600

    all attributes required

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,4 @@
 class Match < ActiveRecord::Base
+  validates :match_id, presence: true
+  validates :time, presence: true
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
+  describe '#attributes' do
+    match = Match.new
+
+    it 'has a match id' do
+      expect(match).to_not be_valid
+      expect(match.errors.keys).to include :match_id
+    end
+
+    it 'has a time' do
+      expect(match).to_not be_valid
+      expect(match.errors.keys).to include :time
+    end
+  end
 end
