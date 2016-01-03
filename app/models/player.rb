b7cd62268e9456fef5085db01bee3b01338928bd
Allen Van <all3nvan@gmail.com>
2016-01-02 18:14:47 -0600

    set default rating and std dev

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,17 @@
+require 'TrueSkill'
+
 class Player < ActiveRecord::Base
   validates :name, presence: true
   validates :summoner_id, presence: true
+
+  before_create :set_default_rating
+
+  private
+
+  def set_default_rating
+    rating = Rating.new
+
+    self.rating = rating.mu
+    self.standard_deviation = rating.sigma
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
+require 'TrueSkill'
 
 RSpec.describe Player, type: :model do
   describe '#attributes' do
     player = Player.new
+    rating = Rating.new
 
     it 'has a name' do
       expect(player).to_not be_valid
@@ -12,6 +14,18 @@ RSpec.describe Player, type: :model do
     it 'has a summoner_id' do
       expect(player).to_not be_valid
       expect(player.errors.keys).to include :summoner_id
+    end
+
+    describe 'trueskill ratings' do
+      valid_player = Player.create(name: 'ayy', summoner_id: 1)
+
+      it 'has trueskill-ranked default rating' do
+        expect(valid_player.rating).to equal(rating.mu)
+      end
+
+      it 'has trueskill-ranked standard deviation' do
+        expect(valid_player.standard_deviation).to equal(rating.sigma)
+      end
     end
   end
 end
