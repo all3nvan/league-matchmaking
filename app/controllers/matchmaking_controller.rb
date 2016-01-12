5228d68a559ad826542f168ffded2c33c28804ed
Allen Van <all3nvan@gmail.com>
2016-01-12 03:16:40 -0600

    flash warning if number of players select isn't 10

--- app/controllers/matchmaking_controller.rb
+++ app/controllers/matchmaking_controller.rb
@@ -1,4 +1,16 @@
 class MatchmakingController < ApplicationController
   def new
+    @players = Player.order(:name).all
+  end
+
+  def create
+    @selected_player_ids = params.select { |k, v| v == 'selected' }.keys.map(&:to_i)
+
+    if @selected_player_ids.size == 10
+      render inline: "<%= @selected_player_ids %>"
+    else
+      flash[:notice] = "Please select 10 players"
+      redirect_to new_matchmaking_path
+    end
   end
 end
