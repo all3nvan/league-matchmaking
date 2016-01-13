c9a47a2af0444f4c39498bb9e350598211ea6549
Allen Van <all3nvan@gmail.com>
2016-01-12 20:20:58 -0600

    matchmaking algorithm

--- app/helpers/matchmaking_helper.rb
+++ app/helpers/matchmaking_helper.rb
@@ -1,2 +1,35 @@
 module MatchmakingHelper
+  def create_teams(selected_player_ids)
+    best_match(selected_player_ids).map do |team|
+      team.map { |player_id| Player.find(player_id) }
+    end
+  end
+
+  private
+
+  def best_match(selected_player_ids)
+    team_combos(selected_player_ids).max_by { |team_combo| match_quality(team_combo) }
+  end
+
+  def team_combos(selected_player_ids)
+    team_1 = selected_player_ids.combination(5).to_a
+    team_2 = team_1.map do |team|
+      selected_player_ids.reject { |id| team.include?(id) }
+    end
+
+    (0..team_1.length - 1).map do |i|
+      [team_1[i], team_2[i]]
+    end
+  end
+
+  def match_quality(team_combo)
+    teams_ratings = team_combo.map do |team|
+      team.map do |player_id|
+        player = Player.find(player_id)
+        Rating.new(player.rating, player.standard_deviation)
+      end
+    end
+
+    g().match_quality(teams_ratings)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'players#index'
-  resources :matchmaking, only: [:new]
+  resources :matchmaking, only: [:new, :create]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
