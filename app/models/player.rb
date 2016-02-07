a7059d987bd281968ac2e9d6e43452ea8f6f1cc3
Allen Van <all3nvan@gmail.com>
2016-02-07 15:00:24 -0800

    show player champion stats

--- app/models/player.rb
+++ app/models/player.rb
@@ -18,6 +18,28 @@ class Player < ActiveRecord::Base
     self.rating - 3 * self.standard_deviation
   end
 
+  def champion_stats
+    player_matches = MatchPlayer.where(player_id: self.id)
+    champion_ids = player_matches
+      .group(:champion_id)
+      .count
+      .keys
+
+    stats = champion_ids.map do |champion_id|
+      champion_matches = player_matches.where(champion_id: champion_id)
+      champion_wins = champion_matches.where(win: true).count
+      champion_losses = champion_matches.where(win: false).count
+      {
+        id: champion_id,
+        wins: champion_wins,
+        losses: champion_losses,
+        winrate: (champion_wins.to_f / (champion_wins + champion_losses) * 100).round(2)
+      }
+    end
+
+    stats.sort_by { |stat| [ stat[:winrate], stat[:wins] ] }.reverse
+  end
+
   private
 
   def set_default_rating
