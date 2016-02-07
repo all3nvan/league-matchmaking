aa91bd805298b9d826d06c7ce173067cc300da6a
Allen Van <all3nvan@gmail.com>
2016-02-07 14:02:57 -0800

    players show route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'players#index'
   resources :matchmaking, only: [:new, :create]
   resources :match, only: [:index]
+  resources :players, only: [:index, :show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
