4eff42547d73aaad13877d0a104634e3f4916cd2
Allen Van <all3nvan@gmail.com>
2016-04-17 00:47:59 -0700

    players show route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :matchmaking, only: [:new], defaults: { format: :json }
   resources :matchmaking, only: [:create]
   resources :match, only: [:index]
-  resources :players, only: [:index], defaults: { format: :json }
+  resources :players, only: [:index, :show], defaults: { format: :json }
   resources :champions, only: [:index]
 
   # Example of regular route:
