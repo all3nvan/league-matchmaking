c9438e0fc624a515ae636de839b42b4cd1d65d8c
Allen Van <all3nvan@gmail.com>
2016-04-17 00:47:11 -0700

    add CORS

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,9 +1,21 @@
 class PlayersController < ApplicationController
+  before_filter :cors_set_access_control_headers
+
   def index
     @players = Player.all.sort_by(&:trueskill).reverse
   end
 
   def show
     @player = Player.find(params[:id])
+  end
+
+  private
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
